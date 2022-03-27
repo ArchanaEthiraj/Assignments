058768c5f4fab96a875c9109c8cf4872aea624b8
ArchanaEthiraj <archana05.doodleblue@gmail.com>
2022-03-27 19:15:57 +0530

    tour

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Tour from './Tour';
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Tour/>
+      </div>
+    )
+  }
+}
+
+export default App
